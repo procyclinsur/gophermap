1cac2760baaf7928dc1d21fe408d87c8b334fb7f
Scott Mattan <pro.cyclinsur@gmail.com>
2019-03-09 04:38:33 +0900

    decode parameter type names for nested structs... (became switch hell..)

--- aster.go
+++ aster.go
@@ -23,28 +23,54 @@ func FindTypes(n ast.Node) ast.Visitor {
 			return VisitorFunc(FindTypes)
 		}
 	case *ast.TypeSpec:
-		switch v := n.Type.(type) {
-		case *ast.StructType:
-			npos := fset.Position(n.Name.Pos())
-			fmt.Println("----")
-			fmt.Printf("line/%d ", npos.Line)
-			fmt.Printf("struct:%s\n", n.Name.Name)
-			for index, item := range v.Fields.List {
-				var fName string
-				if item.Names != nil {
-					fName = item.Names[0].Name
-				} else {
-					fName = "nil"
+		return walkStructSpec(n)
+	}
+	return nil
+}
+
+func fieldNameNilString(n []*ast.Ident) (fName string) {
+	if n != nil {
+		fName = n[0].Name
+	} else {
+		fName = "nil"
+	}
+	return fName
+}
+
+func walkStructSpec(n *ast.TypeSpec) ast.Visitor {
+	switch v := n.Type.(type) {
+	case *ast.StructType:
+		npos := fset.Position(n.Name.Pos())
+		fmt.Println("----")
+		fmt.Printf("line/%d ", npos.Line)
+		fmt.Printf("struct:%s\n", n.Name.Name)
+		for index, item := range v.Fields.List {
+			vpos := fset.Position(v.Fields.Pos())
+			fName := fieldNameNilString(item.Names)
+			var fType string
+			var xSel string
+			var xxName string
+			switch s := item.Type.(type) {
+			case *ast.StarExpr:
+				switch p := s.X.(type) {
+				case *ast.SelectorExpr:
+					xSel = p.Sel.Name
+					switch x := p.X.(type) {
+					case *ast.Ident:
+						xxName = x.Name
+					}
 				}
-				vpos := fset.Position(v.Fields.Pos())
-				fmt.Printf("line:%d ", vpos.Line)
-				fmt.Printf("char:%d ", vpos.Column)
-				fmt.Printf("index:%d ", index)
-				fmt.Printf("parm:%s ", fName)
-				fmt.Printf("type:%s\n", item.Type)
+				fType = xxName + "." + xSel
+			case *ast.Ident:
+				fType = s.Name
 			}
-			return VisitorFunc(FindTypes)
+			fmt.Printf("line:%d ", vpos.Line)
+			fmt.Printf("char:%d ", vpos.Column)
+			fmt.Printf("index:%d ", index)
+			fmt.Printf("parm:%s ", fName)
+			fmt.Printf("type:%s\n", fType)
 		}
+		return VisitorFunc(FindTypes)
 	}
 	return nil
 }
