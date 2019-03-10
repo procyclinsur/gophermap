ff6ac5d43c239cd6427225be16079ecc36bf4a40
Scott Mattan <pro.cyclinsur@gmail.com>
2019-03-10 20:06:11 +0900

    expand sample to test theory

--- examples/testfile.go
+++ examples/testfile.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type test struct {
 	a string
 	b int
@@ -18,4 +20,34 @@ type mixed struct {
 }
 
 type empty struct {
+}
+
+type structMap struct {
+	StructList map[string]StructDef
+}
+
+//StructDef v1.0
+type StructDef struct {
+	//Name of struct
+	Name string
+	//Map of property-name:property-type
+	Properties map[string]string
+	//List of structs contained
+	Contains []string
+}
+
+func main() {
+	struct1 := StructDef{
+		"fakeStruct1",
+		map[string]string{
+			"fakeProperty1": "int",
+			"fakeProperty2": "string",
+			"fakeProperty3": "mixed",
+		},
+		[]string{"mixed"},
+	}
+	var struct2 structMap
+	struct2.StructList = make(map[string]StructDef)
+	struct2.StructList["fakeStruct1"] = struct1
+	fmt.Println(struct2)
 }
